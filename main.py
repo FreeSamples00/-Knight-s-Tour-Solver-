80add6eb53a6680c1a3ca56d9582be4d81b733ca
Spencer Christensen <134820811+FreeSamples00@users.noreply.github.com>
2025-05-04 20:59:55 -0500

    added statistics output

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from solver import ChessBoard, solve_knight_tour, visualize_tour
-
 if __name__ == '__main__':
+    from solver import ChessBoard, solve_knight_tour, visualize_tour
     from sys import argv
+    from time import time
 
     VIS_SPEED = 0.25
     OCCUPIED_CELLS = None
+    OUTPUT_PATH = "./stats.txt"
 
     # validate input
     if len(argv) < 2:
@@ -26,10 +27,27 @@ if __name__ == '__main__':
     board = ChessBoard(argv[1], OCCUPIED_CELLS)
 
     # solve knight's tour
+    t = time()
     solution, num_calls = solve_knight_tour(board)
+    t = time() - t
     
     # if chosen, visualize knight's path via printouts
     choice = input("\nShow visualization? (y/n): ")
     if choice.lower() != 'n':
         visualize_tour(solution, board, VIS_SPEED)
+        print()
+
+    # build stats and write to file
+    stats = ""
+    stats += f"Knight starting location: {solution[0]}\n"
+    stats += f"Knight ending location: {solution[-1]}\n"
+    stats += f"Cells avoided: {OCCUPIED_CELLS}\n"
+    stats += f"Time taken: {t} seconds\n"
+    stats += f"Recursive calls made: {num_calls}\n"
+    stats += f"Tour path: {solution}\n"
+
+    with open(OUTPUT_PATH, 'w') as file:
+        file.write(stats)
+
+    print(f"Stats saved to '{OUTPUT_PATH}'\n")
     
